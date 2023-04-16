77efe0c9d231bb5787c5851d122394c5406104c1
Karri Korsu <karri@DESKTOP-5US45FL>
2023-04-16 15:31:49 +0300

    Added files again?

---
+++ Tutorial/Fov.c
@@ -0,0 +1,159 @@
+/*
+@File    :   Fov.c
+@Time    :   15.04.2023 08:18:49
+@Author  :   Karri Korsu
+@Version :   1.0
+@Contact :   karri.korsu@edu.turkuamk.fi
+@Desc    :   None
+*/
+#include "Rogue.h"
+
+void createFOV(struct Entity* player)
+{
+    int y = 0;
+    int x = 0;
+    int distance = 0;
+    int radius = 15;
+    struct Position target;
+
+    map[player->pos.y][player->pos.x].visible = true;
+    map[player->pos.y][player->pos.x].seen = true;
+
+    for (y = player->pos.y - radius; y < player->pos.y + radius; y++)
+    {
+        for (x = player->pos.x - radius; x < player->pos.x + radius; x++)
+        {
+            target.y = y;
+            target.x = x;
+            distance = getDistance(player->pos, target);
+
+            if (distance < radius && isInMap(y, x) && lineOfSight(player->pos, target))
+            {
+                map[y][x].visible = true;
+                map[y][x].seen = true;
+            }
+        }
+    }
+}
+
+void clearFOV(struct Entity* player)
+{
+    int y = 0;
+    int x = 0;
+    int radius = 15;
+
+    for (y = player->pos.y - radius; y < player->pos.y + radius; y++)
+    {
+        for (x = player->pos.x - radius; x < player->pos.x + radius; x++)
+        {
+            if (isInMap(y,x))
+            {
+                map[y][x].visible = false;
+            }
+        }
+    }
+}
+
+int getDistance(struct Position origin, struct Position target)
+{
+    double dy = 0;
+    double dx = 0;
+    int distance = 0;
+
+    dx = target.x - origin.x;
+    dy = target.y - origin.y;
+    distance = floor(sqrt((dx * dx) + (dy * dy)));
+
+    return distance;
+}
+
+bool isInMap(int y, int x)
+{
+    if((0 < y && y < MAP_HEIGHT - 1) && (0 < x && x < MAP_WIDTH - 1))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool lineOfSight(struct Position origin, struct Position target)
+{
+    int t = 0;
+    int x = 0;
+    int y = 0;
+    int xAbsDelta = 0;
+    int yAbsDelta = 0;
+    int xSign = 0;
+    int ySign = 0;
+    int xDelta = 0;
+    int yDelta = 0;
+
+    xDelta = origin.x - target.x;
+    yDelta = origin.y - target.y;
+
+    xAbsDelta = abs(xDelta);
+    yAbsDelta = abs(yDelta);
+
+    xSign = getSign(xDelta);
+    ySign = getSign(yDelta);
+
+    x = target.x;
+    y = target.y;
+    
+    if (xAbsDelta > yAbsDelta)
+    {
+        t = yAbsDelta * 2 - xAbsDelta;
+
+        while (map[y][x].transparent)
+        {
+            if (t >= 0)
+            {
+                y += ySign;
+                t -= xAbsDelta * 2;
+            }
+
+            x += xSign;
+            t += yAbsDelta * 2;
+
+            if (x == origin.x && y == origin.y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    else
+    {
+        t = xAbsDelta * 2 - yAbsDelta;
+
+        while (map[y][x].transparent)
+        {
+            if (t >= 0)
+            {
+                x += xSign;
+                t -= yAbsDelta * 2;
+            }
+
+            y += ySign;
+            t += xAbsDelta * 2;
+
+            if (x == origin.x && y == origin.y)
+            {
+                return true;
+            }
+        }
+        return false;  
+    }
+}
+
+int getSign(int a)
+{
+    if (a < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
